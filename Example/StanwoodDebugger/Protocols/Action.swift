e28e4bb9e7ca65a94689e1fc07e6161bcbb09040
lukasz-lenkiewicz <l.lenkiewicz@hotmail.com>
2019-01-05 15:04:19 +0100

    Sample Crashes Screen

---
+++ Example/StanwoodDebugger/Protocols/Action.swift
@@ -0,0 +1,15 @@
+//
+//  ActionItem.swift
+//  StanwoodDebugger_Example
+//
+//  Created by Lukasz Lenkiewicz on 05/01/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+protocol ActionItemable {
+    func postAction()
+}
+
+protocol ActionItemCellable {
+    var item: ActionItemable? { get }
+}
