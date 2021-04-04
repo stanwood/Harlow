350833279c44b7ac2e505a4d051437ff4fb7de63
Tal Zion <talezion@gmail.com>
2021-04-04 13:52:13 +0300

    Updating Pulsator SPM

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/shu223/Pulsator.git", .upToNextMajor(from: "0.6.0")),
+        .package(url: "https://github.com/shu223/Pulsator.git", .upToNextMajor(from: "0.6.3")),
         .package(url: "https://github.com/schmidyy/Loaf.git", .upToNextMajor(from: "0.7.0")),
         .package(url: "https://github.com/stanwood/SourceModel_iOS.git", .upToNextMajor(from: "1.3.3"))
     ],
